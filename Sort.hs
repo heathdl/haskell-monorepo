f1349227f6da6b0711fcb6c6edc6219b2b42592d
heathdl <hdavisonlunt@gmail.com>
2025-08-21 05:17:20 +0100

    implement sort test

--- Sort.hs
+++ Sort.hs
@@ -8,3 +8,25 @@ where
 import Sort.BubbleSort (bubbleSort)
 import Sort.MergeSort (mergeSort)
 import Sort.Quicksort (quicksort)
+import System.Random (randomRIO)
+
+generateRandomList :: Int -> IO [Int]
+generateRandomList 0 = return []
+generateRandomList n = do
+  head <- randomRIO (0, 255)
+  tail <- generateRandomList (n - 1)
+  return (head : tail)
+
+applySorts :: [(a -> a -> Bool) -> [a] -> [a]] -> (a -> a -> Bool) -> [a] -> [[a]]
+applySorts sorts compare list = [sort compare list | sort <- sorts]
+
+main :: IO ()
+main = do
+  input <- generateRandomList 16
+  let results = applySorts sorts (<) input
+      output = head results
+  putStrLn ("Unsorted: " ++ show input)
+  putStrLn ("Sorted: " ++ show output)
+  putStrLn ("All implementations match: " ++ show (all (== head results) (tail results)))
+  where
+    sorts = [quicksort, bubbleSort, mergeSort]
