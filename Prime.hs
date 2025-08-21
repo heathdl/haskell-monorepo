be9236cdae253c3bdc6aa9cbec65bed8e4086d0d
heathdl <hdavisonlunt@gmail.com>
2025-08-21 09:39:24 +0100

    improve factorisation and divisor implementations

--- Prime.hs
+++ Prime.hs
@@ -18,32 +18,43 @@ isPrimeTrialDivision 2 = True
 isPrimeTrialDivision 3 = True
 isPrimeTrialDivision value =
   odd value
-    && value `rem` 3 /= 0
+    && value `mod` 3 /= 0
     && go 5 2
   where
     go check increment
       | check * check > value = True
-      | value `rem` check == 0 = False
+      | value `mod` check == 0 = False
       | otherwise = go (check + increment) (6 - increment)
 
 primeDivisors :: Int -> [Int]
-primeDivisors value = filter (divides value) (takeWhile (< value) primes)
+primeDivisors value
+  | value < 2 = []
+  | otherwise = factor value primes
   where
-    divides x y = x `mod` y == 0
-    isLessThanSquareRootOf x y = y * y <= x
+    factor 1 _ = []
+    factor value (divisor : divisors)
+      | divisor * divisor > value = [value]
+      | value `mod` divisor == 0 = divisor : factor (divide value divisor) (divisor : divisors)
+      | otherwise = factor value divisors
+    divide value divisor
+      | value `mod` divisor == 0 = divide (value `div` divisor) divisor
+      | otherwise = value
 
 primeFactorisation :: Int -> [Int]
-primeFactorisation 0 = []
-primeFactorisation 1 = []
 primeFactorisation value
-  | not (null divisors) = first : primeFactorisation (value `div` first)
-  | otherwise = [value]
+  | value < 2 = []
+  | otherwise = factor value primes
   where
-    divisors = primeDivisors value
-    first = head divisors
+    factor 1 _ = []
+    factor value (divisor : divisors)
+      | divisor * divisor > value = [value]
+      | value `mod` divisor == 0 = divisor : factor (value `div` divisor) (divisor : divisors)
+      | otherwise = factor value divisors
 
 main :: IO ()
 main = do
-  print (take 100 (filter isPrimeTrialDivision [1 ..]))
   print (take 100 primes)
-  print (primeFactorisation 21358)
+  print (primeDivisors value)
+  print (primeFactorisation value)
+  where
+    value = 39342594728712960
