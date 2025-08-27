cf97118ba132cff90bb251ccdeef7b1ac7dfb3c9
heathdl <hdavisonlunt@gmail.com>
2025-08-27 10:38:44 +0100

    use integer Newtron-Raphson, implement fractional version of same

--- Square.hs
+++ Square.hs
@@ -1,13 +1,33 @@
-module Square (integerSquareRoot, isPerfectSquare) where
+module Square (integerSquareRoot, isPerfectSquare, newtonRaphsonSquareRoot) where
 
 import BisectionSearch (bisectionSearch)
 import Data.Maybe (fromJust)
+import Debug.Trace (trace)
 
-integerSquareRoot :: Int -> Int
-integerSquareRoot value =
-  fromJust (bisectionSearch (\x -> x * x >= value) 1 value)
-
-isPerfectSquare :: Int -> Bool
-isPerfectSquare value = sqrt * sqrt == value
+integerSquareRoot :: (Integral a) => a -> a
+integerSquareRoot value = go value
   where
-    sqrt = integerSquareRoot value
+    go x
+      | x' == x = x'
+      | otherwise = go x'
+      where
+        x' = (x * x + value) `div` (2 * x)
+
+isPerfectSquare :: (Integral a) => a -> Bool
+isPerfectSquare value = root * root == value
+  where
+    root = integerSquareRoot value
+
+newtonRaphsonSquareRoot :: Rational -> Int -> Rational
+newtonRaphsonSquareRoot value = go value
+  where
+    go x 0 = x
+    go x rounds = 0.5 * (x' + value / x')
+      where
+        x' = go x (rounds - 1)
+
+main :: IO ()
+main = do
+  print (isPerfectSquare (123456789 ^ 2))
+  print (isPerfectSquare (123456789 ^ 2 + 1))
+  print (isPerfectSquare (123456790 ^ 2))
