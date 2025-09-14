38e5acd667fe0f31e7c144ecf62604807a0c2d6d
heathdl <hdavisonlunt@gmail.com>
2025-09-14 05:10:15 +0100

    calculate prime factorisations of sequential numbers (ranging 1 to ∞)

---
+++ Prime/Factorisation/Pairwise.hs
@@ -0,0 +1,54 @@
+module Prime.Factorisation.Pairwise (primeFactorMultisetPairs, primeFactorPairs) where
+
+import Data.List (foldl')
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
+
+createPrimeList :: (Integral a) => (Map a [b] -> a -> Maybe [a] -> [b]) -> [(a, [b])]
+createPrimeList formatFactors = (1, []) : go 2 Map.empty (Map.singleton 1 [])
+  where
+    go n !compositeMap !factorMap =
+      (n, factors) : go (n + 1) compositeMap' factorMap'
+      where
+        (factors, compositeMap') =
+          maybe
+            (handlePrime compositeMap factorMap n)
+            (handleComposite compositeMap factorMap n)
+            (Map.lookup n compositeMap)
+        factorMap' = Map.insert n factors factorMap
+
+    handlePrime compositeMap factorMap p = (factors, compositeMap')
+      where
+        factors = formatFactors factorMap p Nothing
+        compositeMap' = Map.insertWith (++) (p * p) [p] compositeMap
+
+    handleComposite compositeMap factorMap n ps@(p : _) = (factors, compositeMap')
+      where
+        factors = formatFactors factorMap n (Just ps)
+        compositeMap' =
+          foldl'
+            (\mp pr -> Map.insertWith (++) (n + pr) [pr] mp)
+            (Map.delete n compositeMap)
+            ps
+
+primeFactorPairs :: (Integral a) => [(a, [(a, Int)])]
+primeFactorPairs = createPrimeList formatFactors
+  where
+    formatFactors factorMap n Nothing = [(n, 1)]
+    formatFactors factorMap n (Just ps@(p : _)) = factors
+      where
+        knownFactors = factorMap Map.! (n `div` p)
+        factors = case knownFactors of
+          ((q, count) : rest) | p == q -> (q, count + 1) : rest
+          _ -> (p, 1) : knownFactors
+
+primeFactorMultisetPairs :: (Integral a) => [(a, [a])]
+primeFactorMultisetPairs = createPrimeList formatFactors
+  where
+    formatFactors factorMap n Nothing = [n]
+    formatFactors factorMap n (Just ps@(p : _)) = p : factorMap Map.! (n `div` p)
+
+main :: IO ()
+main = do
+  print (take 100 (map (product . snd) primeFactorMultisetPairs))
+  print (take 100 primeFactorPairs)
