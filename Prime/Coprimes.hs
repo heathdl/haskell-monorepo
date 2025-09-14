74c9e797dc9adb998856f97de7e047cd8355051c
heathdl <hdavisonlunt@gmail.com>
2025-09-14 05:49:57 +0100

    implement coprimality tests & lists

---
+++ Prime/Coprimes.hs
@@ -0,0 +1,33 @@
+module Prime.Coprimes (areFactorisationsCoprime, areCoprime, calculateCoprimes, compositeCoprimes) where
+
+import Prime.Factorisation (primeFactorisation)
+import Prime.Factorisation.Pairwise (primeFactorPairs)
+
+areFactorisationsCoprime :: (Integral a) => [(a, Int)] -> [(a, Int)] -> Bool
+areFactorisationsCoprime [] _ = True
+areFactorisationsCoprime _ [] = True
+areFactorisationsCoprime a@((ah, _) : as) b@((bh, _) : bs)
+  | ah < bh = areFactorisationsCoprime as b
+  | ah > bh = areFactorisationsCoprime b bs
+  | otherwise = False
+
+areCoprime :: (Integral a) => a -> a -> Bool
+areCoprime a 0 = a == 1 || a == -1
+areCoprime a b = areCoprime b (a `mod` b)
+
+calculateCoprimes :: (Integral a) => a -> [a]
+calculateCoprimes x = map fst (filter (areFactorisationsCoprime factorisation . snd) candidates)
+  where
+    factorisation = primeFactorisation x
+    candidates = takeWhile (\a -> x > fst a) primeFactorPairs
+
+compositeCoprimes :: (Integral a) => a -> [a]
+compositeCoprimes x = map fst (filter isCompositeCoprime candidates)
+  where
+    factorisation = primeFactorisation x
+    candidates = takeWhile (\(a, _) -> a < x) primeFactorPairs
+    isCompositeCoprime (_, b) = length b > 1 && areFactorisationsCoprime factorisation b
+
+main :: IO ()
+main = do
+  print (compositeCoprimes 35776)
