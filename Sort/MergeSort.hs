8b5879c5bdbc6683458f76e1bab7fa9c38b14834
heathdl <hdavisonlunt@gmail.com>
2025-09-14 05:10:48 +0100

    add mergeAll, export merge

--- Sort/MergeSort.hs
+++ Sort/MergeSort.hs
@@ -1,5 +1,17 @@
 module Sort.MergeSort where
 
+merge :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+merge _ [] y = y
+merge _ x [] = x
+merge compare (x : xs) (y : ys)
+  | compare x y = x : merge compare xs (y : ys)
+  | otherwise = y : merge compare (x : xs) ys
+
+mergeAll :: (a -> a -> Bool) -> [[a]] -> [a]
+mergeAll _ [] = []
+mergeAll _ [xs] = xs
+mergeAll compare (xs : ys : rest) = mergeAll compare (merge compare xs ys : rest)
+
 mergeSort :: (a -> a -> Bool) -> [a] -> [a]
 mergeSort _ [] = []
 mergeSort _ [x] = [x]
@@ -9,9 +21,3 @@ mergeSort compare list =
     half = length list `div` 2
     first = take half list
     second = drop half list
-
-    merge _ [] y = y
-    merge _ x [] = x
-    merge compare (x : xTail) (y : yTail)
-      | compare x y = x : merge compare xTail (y : yTail)
-      | otherwise = y : merge compare (x : xTail) yTail
