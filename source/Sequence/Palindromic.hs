1e8978a30c98aba7e341542e2ac6c345afe7d17f
heathdl <hdavisonlunt@gmail.com>
2025-09-17 09:01:13 +0100

    implement palindrome related sequences

---
+++ source/Sequence/Palindromic.hs
@@ -0,0 +1,28 @@
+module Sequence.Palindromic where
+
+import Digits (digitsFromIntegral)
+import NumericalPalindromes (getPalindromicBasesOf)
+import Prime (primes)
+import ScriptedNumbers (toSubscript)
+
+strictlyNonPalindromicNumbers :: (Integral a) => [a]
+strictlyNonPalindromicNumbers = 0 : 1 : 2 : 3 : 4 : 6 : filter (null . getPalindromicBasesOf) (drop 2 primes)
+
+highlyPalindromicNumbers :: (Integral a) => [a]
+highlyPalindromicNumbers = 3 : 5 : 10 : 21 : 36 : 60 : 80 : highlyPalindromicNumbers [120, 132 ..] 6
+  where
+    highlyPalindromicNumbers (x : xs) currentHighest
+      | factorCount > currentHighest = x : highlyPalindromicNumbers xs factorCount
+      | otherwise = highlyPalindromicNumbers xs currentHighest
+      where
+        bases = getPalindromicBasesOf x
+        factorCount = length bases
+
+main :: IO ()
+main = do
+  mapM_ (\x -> putStrLn (show x ++ " = " ++ displayPalindromicForms x)) highlyPalindromicNumbers
+  where
+    displayPalindromicForms x = take (length str - 1) str
+      where
+        str = concatMap (displayInBase x) (getPalindromicBasesOf x)
+    displayInBase x b = show (digitsFromIntegral b x) ++ toSubscript b ++ " "
