ca7520625a698a195e9445ccf20b3f1fac534962
heathdl <hdavisonlunt@gmail.com>
2025-09-20 14:53:55 +0100

    infinite (sorted) lists of integers with `n` 1 bits (relevant to choosing n elements out of a set of k elements)

---
+++ source/Sequence/BinaryHammingWeight.hs
@@ -0,0 +1,39 @@
+module Sequence.BinaryHammingWeight where
+
+import BinaryDisplay (showAutoJustifiedBinaryList, showBinaryList, showJustifiedBinaryList)
+import BinaryDisplay.DisplayTypes (redColour)
+import Data.Bits (Bits (shiftR, xor, (.&.), (.|.)))
+
+iterativeHakmem175O :: Int -> [Word]
+iterativeHakmem175O n = iterate next (2 ^ n - 1)
+  where
+    next x = r .|. o'
+      where
+        s = x .&. negate x
+        r = x + s
+        o = r `xor` x
+        o' = (o `shiftR` 2) `div` s
+
+binaryHammingWeightOf :: (Integral a) => Int -> [a]
+binaryHammingWeightOf 0 = [0]
+binaryHammingWeightOf 1 = iterate (* 2) 1 -- not required, presumably improves performance
+binaryHammingWeightOf n = go (2 ^ (n - 1))
+  where
+    go offset = map (offset +) (takeWhile (< offset) previous) ++ go (offset * 2)
+    previous = binaryHammingWeightOf (n - 1)
+
+chooseNOfK :: (Integral a) => a -> a -> [[a]]
+chooseNOfK 0 _ = [[]]
+chooseNOfK n k
+  | n <= k = [x : xs | x <- [0 .. k - 1], xs <- chooseNOfK (n - 1) x]
+  | otherwise = []
+
+main :: IO ()
+main = do
+  putStrLn (showJustifiedBinaryList redColour k bitmaps)
+  print bitmaps
+  print (map (foldr (\x -> (+) (2 ^ x)) 0) (chooseNOfK n k))
+  where
+    n = 4
+    k = 10
+    bitmaps = takeWhile (<= 2 ^ k) (binaryHammingWeightOf n) :: [Integer]
