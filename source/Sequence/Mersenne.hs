8c1aaa261597bdca49fdb109405a245dcc958431
heathdl <hdavisonlunt@gmail.com>
2025-09-23 11:57:04 +0100

    implement Lucas Lehmer primality test

--- source/Sequence/Mersenne.hs
+++ source/Sequence/Mersenne.hs
@@ -1,14 +1,27 @@
-module Sequence.Mersenne (mersenneNumbers, mersennePrimes) where
+module Sequence.Mersenne (lucasLehmer, mersenneNumbers, mersennePrimePairs, mersennePrimes, mersenneExponents) where
 
 import Prime (isPrime)
 
-mersenneNumbers :: (Integral a) => [a]
-mersenneNumbers = iterate (\x -> x * 2 + 1) 3
+lucasLehmer :: (Integral a) => a -> a -> Bool
+lucasLehmer 2 _ = True
+lucasLehmer n m = go (n - 2) 4
+  where
+    go 0 s = s `mod` m == 0
+    go k s = go (k - 1) ((s * s - 2) `mod` m)
+
+mersenneNumbers :: (Integral a) => [(a, a)]
+mersenneNumbers = iterate (\(x, e) -> (x * 2 + 1, e + 1)) (3, 2)
+
+mersennePrimePairs :: (Integral a) => [(a, a)]
+mersennePrimePairs = filter (uncurry (flip lucasLehmer)) mersenneNumbers
 
 mersennePrimes :: (Integral a) => [a]
-mersennePrimes = filter isPrime mersenneNumbers
+mersennePrimes = map fst mersennePrimePairs
+
+mersenneExponents :: (Integral a) => [a]
+mersenneExponents = map snd mersennePrimePairs
 
 main :: IO ()
 main = do
-  print (take 15 mersenneNumbers :: [Integer])
-  print (mersennePrimes :: [Integer])
+  print (take 15 mersenneNumbers)
+  mapM_ print (mersennePrimes :: [Integer])
