40b3b2432724b6cf1132f7494e0a3462351fdf10
heathdl <hdavisonlunt@gmail.com>
2025-10-08 18:03:57 +0100

    move EulersTotient.hs to Totients.hs and implement Carmichael's Totient

--- source/Prime/EulersTotient.hs
+++
@@ -1,9 +0,0 @@
-module Prime.EulersTotient (eulersTotient, eulersTotientGivenFactorisation) where
-
-import Prime.Factorisation (primeFactorisation)
-
-eulersTotientGivenFactorisation :: (Integral a) => [(a, Int)] -> a
-eulersTotientGivenFactorisation factorisation = product (map (\(factor, count) -> (factor - 1) * factor ^ (count - 1)) factorisation)
-
-eulersTotient :: (Integral a) => a -> a
-eulersTotient = eulersTotientGivenFactorisation . primeFactorisation
---
+++ source/Prime/Totients.hs
@@ -0,0 +1,44 @@
+module Prime.Totients (eulersTotient, eulersTotientGivenFactorisation, carmichaelsTotient, carmichaelsTotientGivenFactorisation, carmichaelsTotientOfPrimes) where
+
+import Prime.Factorisation (primeFactorisation)
+
+eulersTotientGivenFactorisation :: (Integral a) => [(a, Int)] -> a
+eulersTotientGivenFactorisation factorisation = product (map (\(factor, count) -> (factor - 1) * factor ^ (count - 1)) factorisation)
+
+eulersTotient :: (Integral a) => a -> a
+eulersTotient = eulersTotientGivenFactorisation . primeFactorisation
+
+carmichaelsTotientGivenFactorisation :: (Integral a) => [(a, Int)] -> a
+carmichaelsTotientGivenFactorisation factorisation
+  | null factorisation || factorisation == [(2, 1)] || factorisation == [(2, 2)] = totient
+  | isPowerOfTwo = totient `div` 2
+  | isPowerOfPrime = totient
+  | otherwise = leastCommonMultiple (map (\x -> carmichaelsTotientGivenFactorisation [x]) factorisation)
+  where
+    isPowerOfPrime = null (tail factorisation)
+    isPowerOfTwo = case factorisation of
+      [(2, c)] -> c >= 3
+      _ -> False
+    totient = eulersTotientGivenFactorisation factorisation
+
+    leastCommonMultiple c = go c
+      where
+        incrementIfLess highest h@(v, c)
+          | v < highest = v + c
+          | otherwise = v
+        go a
+          | isFinished = head a
+          | otherwise = go (zipWith (curry (incrementIfLess largest)) a c)
+          where
+            isFinished = all (== head a) (tail a)
+            largest = foldl max 0 a
+
+carmichaelsTotient :: (Integral a) => a -> a
+carmichaelsTotient a = carmichaelsTotientGivenFactorisation (primeFactorisation a)
+
+carmichaelsTotientOfPrimes :: (Integral a) => a -> a -> a
+carmichaelsTotientOfPrimes p q = (a `div` g) * b
+  where
+    a = p - 1
+    b = q - 1
+    g = gcd a b
