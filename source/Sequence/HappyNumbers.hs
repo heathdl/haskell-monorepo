0390139f7226db14b0190316b0aec821de906c72
heathdl <hdavisonlunt@gmail.com>
2025-10-11 09:10:33 +0100

    happy numbers

---
+++ source/Sequence/HappyNumbers.hs
@@ -0,0 +1,72 @@
+module Sequence.HappyNumbers where
+
+import Data.IntMap.Strict qualified as IntMap
+import Data.IntSet qualified as IntSet
+import Data.List (foldl')
+
+isHappy :: Int -> Bool
+isHappy x = go x (IntSet.singleton x)
+  where
+    go 1 visited = True
+    go x visited
+      | IntSet.member x' visited = False
+      | otherwise = go x' (IntSet.insert x' visited)
+      where
+        x' = sumDigitsSquared x
+
+happyNumbers :: [Int]
+unhappyNumbers :: [Int]
+(happyNumbers, unhappyNumbers) = go IntSet.empty IntSet.empty [1 ..]
+  where
+    go happySet unhappySet (x : xs) =
+      case inner x IntSet.empty of
+        (True, visited) -> (x : hs, us)
+          where
+            (hs, us) = go (IntSet.union happySet visited) unhappySet xs
+        (False, visited) -> (hs, x : us)
+          where
+            (hs, us) = go happySet (IntSet.union unhappySet visited) xs
+      where
+        inner 1 visited = (True, IntSet.insert 1 visited)
+        inner n visited
+          | IntSet.member n happySet = (True, visited)
+          | IntSet.member n unhappySet = (False, visited)
+          | IntSet.member n visited = (False, visited)
+          | otherwise = inner n' (IntSet.insert n visited)
+          where
+            n' = sumDigitsSquared n
+
+happyNumberTerminals :: [Int]
+happyNumberTerminals = go IntMap.empty [1 ..]
+  where
+    go !cache (x : xs) = t : go cache' xs
+      where
+        (t, cache') = case IntMap.lookup x cache of
+          Just t -> (t, cache)
+          Nothing -> inner x cache IntMap.empty []
+
+    inner 1 cache visited path = (1, foldl' (\m v -> IntMap.insert v 1 m) (IntMap.insert 1 1 cache) path)
+    inner n cache visited path
+      | Just t <- IntMap.lookup n cache = (t, foldl' (\m v -> IntMap.insert v t m) cache path)
+      | Just i <- IntMap.lookup n visited =
+          let (prefix, cycleMembers) = splitAt i path
+              entry = head cycleMembers
+              cache1 = foldl' (\m v -> IntMap.insert v v m) cache cycleMembers
+              cache2 = foldl' (\m v -> IntMap.insert v entry m) cache1 prefix
+           in (entry, cache2)
+      | otherwise =
+          let visited' = IntMap.insert n (length path) visited
+              path' = path ++ [n]
+              n' = sumDigitsSquared n
+           in inner n' cache visited' path'
+
+sumDigitsSquared :: (Integral a) => a -> a
+sumDigitsSquared 0 = 0
+sumDigitsSquared n = r * r + sumDigitsSquared q
+  where
+    (q, r) = n `quotRem` 10
+
+main :: IO ()
+main = do
+  mapM_ (\(a, b) -> putStrLn (show a ++ " " ++ show b)) (zip [1 ..] (takeWhile (<= 1000) happyNumbers))
+  print (take 20 happyNumberTerminals)
