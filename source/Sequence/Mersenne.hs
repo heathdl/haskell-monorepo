5322c6c13ea98de7bec02c7cc49b04c54476b618
heathdl <hdavisonlunt@gmail.com>
2025-10-11 09:11:04 +0100

    don't use lucas lehmer on non-prime exponents

--- source/Sequence/Mersenne.hs
+++ source/Sequence/Mersenne.hs
@@ -1,6 +1,6 @@
 module Sequence.Mersenne (lucasLehmer, mersenneNumbers, mersennePrimePairs, mersennePrimes, mersenneExponents) where
 
-import Prime (isPrime)
+import Prime (isPrime, primes)
 
 lucasLehmer :: (Integral a) => a -> a -> Bool
 lucasLehmer 2 _ = True
@@ -10,10 +10,18 @@ lucasLehmer n m = go (n - 2) 4
     go k s = go (k - 1) ((s * s - 2) `mod` m)
 
 mersenneNumbers :: (Integral a) => [(a, a)]
-mersenneNumbers = iterate (\(x, e) -> (x * 2 + 1, e + 1)) (3, 2)
+mersenneNumbers = iterate (\(x, e) -> (x + x + 1, e + 1)) (3, 2)
+
+mersennePrimeCandidates :: (Integral a) => [(a, a)]
+mersennePrimeCandidates = go mersenneNumbers primes
+  where
+    go ms@(m@(_, e) : ms') ps@(p : ps')
+      | e == p = m : go ms' ps'
+      | e > p = go ms ps'
+      | otherwise = go ms' ps
 
 mersennePrimePairs :: (Integral a) => [(a, a)]
-mersennePrimePairs = filter (uncurry (flip lucasLehmer)) mersenneNumbers
+mersennePrimePairs = filter (uncurry (flip lucasLehmer)) mersennePrimeCandidates
 
 mersennePrimes :: (Integral a) => [a]
 mersennePrimes = map fst mersennePrimePairs
