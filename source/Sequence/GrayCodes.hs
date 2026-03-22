4e505296dab687f5733b3e5a40edd0d4386e8d2a
heathdl <hdavisonlunt@gmail.com>
2026-03-22 12:29:09 +0000

    add nth greycode

--- source/Sequence/GrayCodes.hs
+++ source/Sequence/GrayCodes.hs
@@ -2,9 +2,13 @@ module Sequence.GrayCodes where
 
 import BinaryDisplay (showAutoJustifiedBinaryList)
 import BinaryDisplay.DisplayTypes (blueColour)
+import Data.Bits (Bits, shiftR, xor)
 
 grayCodes :: (Integral a) => [a]
 grayCodes = concat nBitGrayCodes
+
+grayCode :: (Integral a, Bits a) => a -> a
+grayCode n = n `xor` (n `shiftR` 1)
 
 nBitGrayCodes :: (Integral a) => [[a]]
 nBitGrayCodes = map firstOfThree (iterate extendGrayCodes ([0], [], 1))
