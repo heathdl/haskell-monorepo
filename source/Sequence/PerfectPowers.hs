d4eabee1def40315fd7705f6fb1cc3dd6e567c90
heathdl <hdavisonlunt@gmail.com>
2026-03-22 12:29:17 +0000

    add isPerfectPower

--- source/Sequence/PerfectPowers.hs
+++ source/Sequence/PerfectPowers.hs
@@ -5,6 +5,16 @@ import Merge (mergeStrictlyIncreasing)
 perfectPowers :: (Integral a) => [a]
 perfectPowers = mergeStrictlyIncreasing powers
 
+isPerfectPower :: (Integral a) => a -> Bool
+isPerfectPower n
+  | n < 4 = False
+  | otherwise = any ((== 1) . divideOutOf n) bases
+  where
+    bases = takeWhile (\b -> b * b <= n) [2 ..]
+    divideOutOf n a
+      | n `mod` a == 0 = divideOutOf (n `div` a) a
+      | otherwise = n
+
 powers :: (Integral a) => [[a]]
 powers = [iterate (* k) (k * k) | k <- [2 ..]]
 
