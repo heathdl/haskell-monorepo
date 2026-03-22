eb961b5e054802a33da1dd066048d6b7212e8ef7
heathdl <hdavisonlunt@gmail.com>
2026-03-22 12:29:53 +0000

    nth catalan number

--- source/Sequence/Catalan.hs
+++ source/Sequence/Catalan.hs
@@ -1,10 +1,15 @@
-module Sequence.Catalan (catalanNumbers) where
+module Sequence.Catalan (catalanNumbers, catalanNumber) where
 
 -- https://oeis.org/A000108
 catalanNumbers :: (Integral a) => [a]
 catalanNumbers = scanl step 1 [1 ..]
   where
     step previous x = previous * (4 * x - 2) `div` (x + 1)
+
+catalanNumber :: (Integral a) => a -> a
+catalanNumber n = binomial (2 * n) n `div` (n + 1)
+  where
+    binomial n k = product [k + 1 .. n] `div` product [1 .. n - k]
 
 main :: IO ()
 main = do
